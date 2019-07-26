1107bdc20b5832ffeb33663a6e5f443de95c4541
Mathieu Lecarme <mlecarme@bearstech.com>
2019-07-26 16:17:51 +0200

    Commenting deadman.

--- deadman/deadman.go
+++ deadman/deadman.go
@@ -4,32 +4,38 @@ import (
 	"github.com/willf/bitset"
 )
 
+// DeadRegistry stores the dead and the living
 type DeadRegistry struct {
 	bitset *bitset.BitSet
 }
 
+// NewDeadRegistry return DeadRegistry with a fixed size
 func NewDeadRegistry(size uint) *DeadRegistry {
 	return &DeadRegistry{
 		bitset: bitset.New(size),
 	}
 }
 
+// Alive set ranked buddy as alive
 func (d *DeadRegistry) Alive(rank uint) *DeadRegistry {
 	d.bitset.Set(rank)
 	return d
 }
 
+// Reset all the buddies to alive
 func (d *DeadRegistry) Reset() *DeadRegistry {
 	d.bitset.ClearAll()
 	return d
 }
 
-func (d1 *DeadRegistry) Or(d2 *DeadRegistry) *DeadRegistry {
+// Or is a boolean operation on two DeadRegistries
+func (d *DeadRegistry) Or(d2 *DeadRegistry) *DeadRegistry {
 	return &DeadRegistry{
-		bitset: d1.bitset.Union(d2.bitset),
+		bitset: d.bitset.Union(d2.bitset),
 	}
 }
 
+// DeadIterator over dead buddies
 type DeadIterator struct {
 	registry *DeadRegistry
 	cpt      uint
@@ -46,6 +52,7 @@ func (di *DeadIterator) Next() (uint, bool) {
 	return rank, ok
 }
 
+// DeadIterator returns a DeadIterator
 func (d *DeadRegistry) DeadIterator() *DeadIterator {
 	return &DeadIterator{
 		registry: d,
--- deadman/genealogy.go
+++ deadman/genealogy.go
@@ -1,10 +1,12 @@
 package deadman
 
+// Genealogy stores deads over time
 type Genealogy struct {
 	genealogies []*DeadRegistry
 	rank        int // Age
 }
 
+// New Genealogy with a size and a number of generation
 func New(generation int, size uint) *Genealogy {
 	genealogies := make([]*DeadRegistry, generation)
 	for i := 0; i < generation; i++ {
@@ -15,6 +17,7 @@ func New(generation int, size uint) *Genealogy {
 	}
 }
 
+// Tick add one more generation
 func (g *Genealogy) Tick() {
 	// FIXME it's not threadsafe
 	g.rank++
@@ -23,6 +26,7 @@ func (g *Genealogy) Tick() {
 	}
 }
 
+// Current generation
 func (g *Genealogy) Current() *DeadRegistry {
 	return g.genealogies[g.rank]
 }
@@ -32,6 +36,7 @@ func (g *Genealogy) previous(n int) int {
 	return (g.rank + l - n) % l
 }
 
+// Previous generation
 func (g *Genealogy) Previous(n int) *DeadRegistry {
 	// return nil if n > size
 	if n >= (len(g.genealogies)-1) || n < 0 {
@@ -40,6 +45,7 @@ func (g *Genealogy) Previous(n int) *DeadRegistry {
 	return g.genealogies[g.previous(n)]
 }
 
+// Crunch n generations with a logical OR
 func (g *Genealogy) Crunch(n int) *DeadRegistry {
 	if n >= (len(g.genealogies)-1) || n <= 0 {
 		return nil
