83a2e6a609aeb2e31af550ff42598199fa54cb8b
Mathieu Lecarme <mlecarme@bearstech.com>
2019-08-08 16:49:57 +0200

    Testing stuffs with expr.

--- rule/evaluator_test.go
+++ rule/evaluator_test.go
@@ -25,3 +25,35 @@ func TestExprJSON(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, float64(40), r)
 }
+
+func TestNil(t *testing.T) {
+	r, err := _expr.Eval(`a`, map[string]interface{}{})
+	assert.NoError(t, err)
+	assert.Equal(t, nil, r)
+}
+
+func TestFunc(t *testing.T) {
+	r, err := _expr.Eval(`add(41, 1)`, map[string]interface{}{
+		"add": func(i, j int) interface{} {
+			return i + j
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 42, r)
+}
+
+func TestDefault(t *testing.T) {
+	// only ternary operator can handle nil
+	r, err := _expr.Eval(`a != nil ? a : 42`, map[string]interface{}{})
+	assert.NoError(t, err)
+	assert.Equal(t, 42, r)
+	r, err = _expr.Eval(`default(a, 42)`, map[string]interface{}{
+		"default": func(v, def interface{}) interface{} {
+			if v == nil {
+				return def
+			}
+			return v
+		},
+	})
+	assert.Error(t, err)
+}
