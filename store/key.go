2fe409c09b1015bd2e61285ddf98138ba7f7f57b
Mathieu Lecarme <mlecarme@bearstech.com>
2019-08-16 16:26:39 +0200

    Fighting with key.

--- store/key.go
+++ store/key.go
@@ -6,11 +6,8 @@ import (
 	"github.com/influxdata/influxdb/models"
 )
 
-// Key is a combined key, for storing timeline.Timeline in Store
-type Key string
-
 // BuildKey returns Key from a list of tag name, and an Influxdb point
-func BuildKey(keys []string, point models.Point) Key {
+func BuildKey(keys []string, point models.Point) string {
 	b := bytes.Buffer{}
 	b.Write(point.Name())
 	for _, key := range keys {
@@ -19,5 +16,5 @@ func BuildKey(keys []string, point models.Point) Key {
 		b.WriteRune('=')
 		b.Write(point.Tags().Get([]byte(key)))
 	}
-	return Key(b.String())
+	return b.String()
 }
--- store/store.go
+++ store/store.go
@@ -1,28 +1,29 @@
 package store
 
 import (
+	"github.com/armon/go-radix"
 	"github.com/factorysh/gyumao/timeline"
 	"github.com/influxdata/influxdb/models"
 )
 
 // Store stores timeline.Timeline
 type Store struct {
-	store map[Key]*timeline.Timeline
+	store *radix.Tree
 }
 
 // New returns a new Store
 func New() *Store {
 	return &Store{
-		store: make(map[Key]*timeline.Timeline),
+		store: radix.New(),
 	}
 }
 
 // Append a new Influxdb point, with the names of the keys
 func (s *Store) Append(keys []string, point models.Point) {
 	k := BuildKey(keys, point)
-	_, ok := s.store[k]
+	tl, ok := s.store.Get(k)
 	if !ok {
-		s.store[k] = timeline.New(3) // FIXME hardcoded length
+		s.store.Insert(k, timeline.New(3)) // FIXME hardcoded length
 	}
-	s.store[k].Set(point.Time(), point)
+	tl.(*timeline.Timeline).Set(point.Time(), point)
 }
