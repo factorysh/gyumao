1877d9f11e8ed20a459e742a3e477a6013a98632
Mathieu Lecarme <mlecarme@bearstech.com>
2019-08-28 21:05:29 +0200

    More config.

--- mockup.sh
+++ mockup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -XPOST "http://localhost:8066/write" --data-binary 'http,status=200 rtime=60'
+curl -XPOST "http://localhost:8066/write" \
+    --data-binary 'http,status=200,hostname=alice rtime=60'
